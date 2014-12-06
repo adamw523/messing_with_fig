f85727ddf4d223269e3066f3918dc84c4b8c24e1
Adam Wisniewski <adamw@tbcn.ca>
2014-12-06 17:10:54 -0500

    adding templates, some create / delete functionality

--- web/app.py
+++ web/app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from db import db, Tick
+from flask import Flask, redirect, render_template, request, url_for
+import flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'hello there'
+def index():
+    return render_template('index.html')
+
+@app.route('/ticks', methods=['GET', 'POST'])
+def ticks():
+    if request.method == 'POST':
+        t = Tick(message=request.form['message'])
+        db.session.add(t)
+        db.session.commit()
+
+    return render_template('ticks.html', ticks=Tick.query.all())
+
+@app.route('/ticks/<int:id>/delete')
+def ticks_delete(id=None):
+    t = Tick.query.get(id)
+    db.session.delete(t)
+    db.session.commit()
+    return redirect(url_for('ticks'))
+
+@app.route('/ticks/create_all')
+def ticks_create_all():
+    db.create_all()
+    return redirect(url_for('index'))
 
 app.debug = True
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
 
